85ee933fd69f4c77a8f6fdee40875870893df918
Helder Castrillón <helder_947@hotmail.com>
2018-08-30 11:02:04 -0500

    Update and rename Mensaje to Mensaje.js

---
+++ Mensaje.js
@@ -0,0 +1,19 @@
+{
+    "subject": "Hey",
+    "text": "How are you?",
+    "users": [
+        {
+            "id": "OYLGMiazHtW"
+        }
+    ],
+    "userGroups": [
+        {
+            "id": "ZoHNWQajIoe"
+        }
+    ],
+    "organisationUnits": [
+        {
+            "id": "DiszpKrYNg8"
+        }
+    ]
+}
