3f91c79dda176147b19053b947b79ae0921885d4
Helder Castrillón <helder_947@hotmail.com>
2018-08-30 11:56:33 -0500

    Update curl_demo.cmd

--- curl_demo.cmd
+++ curl_demo.cmd
@@ -1,1 +1,1 @@
-curl -d @nombreArchivo.json "https://dev.psi-mis.org/api/XXXXXXX" -X POST -H "Content-Type: application/json" -u usuario:contraseña -v -k -o archivoSalida.json
+curl -d @mensaje.json "https://tot.dhis2.org/training1/api/messageConversations" -X POST -H "Content-Type: application/json" -u usuario:Contraseña -v -k -o salida.json
